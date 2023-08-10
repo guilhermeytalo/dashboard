1e41f9d5cc64f0901e1aad914b561625ce6b77b5
Guilherme Ytalo <gydaytalo@gmail.com>
2023-08-10 17:34:38 -0300

    first commit

---
+++ src/components/sidemenu/index.tsx
@@ -0,0 +1,7 @@
+export const SideMenu = () => {
+  return (
+    <div>
+      <h2>Side Menu</h2>
+    </div>
+  );
+};
---
+++ src/pages/App.tsx
@@ -0,0 +1,124 @@
+import { Avatar, Box, Card, Grid, Text } from '@radix-ui/themes';
+
+function App() {
+  return (
+    <Grid columns="2" rows="2" gap="8" width="auto" align="center" justify="center">
+      <Box height="9">
+        <Card>
+          <Avatar
+            size="3"
+            src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+            radius="full"
+            fallback="T"
+          />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              Teodros Girmay
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>
+        </Card>
+      </Box>
+
+      <Box height="9">
+        <Card>
+          <Avatar
+            size="3"
+            src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+            radius="full"
+            fallback="T"
+          />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              Teodros Girmay
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>
+        </Card>
+      </Box>
+
+      <Box height="9">
+        <Card>
+          <Avatar
+            size="3"
+            src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+            radius="full"
+            fallback="T"
+          />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              Teodros Girmay
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>
+        </Card>
+      </Box>
+
+      <Box height="9">
+        <Card>
+          <Avatar
+            size="3"
+            src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+            radius="full"
+            fallback="T"
+          />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              Teodros Girmay
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>
+        </Card>
+      </Box>
+
+      <Box height="9">
+        <Card>
+          <Avatar
+            size="3"
+            src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+            radius="full"
+            fallback="T"
+          />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              Teodros Girmay
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>{' '}
+        </Card>
+      </Box>
+
+      <Box height="9">
+        <Card>
+          <Avatar
+            size="3"
+            src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+            radius="full"
+            fallback="T"
+          />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              Teodros Girmay
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>
+        </Card>
+      </Box>
+    </Grid>
+
+  );
+}
+
+export default App;
